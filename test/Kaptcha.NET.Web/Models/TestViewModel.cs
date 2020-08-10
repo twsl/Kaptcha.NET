f9c81ba4cf16687c2087a432a8f642ef626cfced
twsl <45483159+twsl@users.noreply.github.com>
2020-08-10 14:27:12 +0200

    Removed website example from test folder

--- test/Kaptcha.NET.Web/Models/TestViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KaptchaNET.Web.Models
-{
-    public class TestViewModel
-    {
-        public string TestValue { get; set; }
-        public string TestValueLink { get; set; }
-    }
-}
